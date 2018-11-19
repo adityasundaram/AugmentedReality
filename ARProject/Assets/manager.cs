7158f560fca2ea16abf68a98461c1008d36887ab
adityasundaram <aditya1sundaram@gmail.com>
2018-11-18 20:02:21 -0800

    Added a simple start splash screen

---
+++ ARProject/Assets/manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class manager : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject tag;
+    private Animator titleAnim;
+    private Animator tagAnim;
+    private bool slideDown;
+    // Use this for initialization
+    void Start()
+    {
+        titleAnim = title.GetComponent<Animator>();
+        tagAnim = tag.GetComponent<Animator>();
+        titleAnim.SetBool("isHidden", false);
+        tagAnim.SetBool("isHidden", false);
+        slideDown = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTouches();
+    }
+
+    void ProcessTouches()
+    {
+        Touch touch;
+        if (Input.touchCount != 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+        else
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
